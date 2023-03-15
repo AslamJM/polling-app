074238d6a7ba53b5cd9eb89929a51bb1061bdd8a
AslamJM <jma8744@gmail.com>
2023-03-15 11:45:22 +0530

    added create poll components/ zustand

--- src/components/home/CreatePoll.tsx
+++ src/components/home/CreatePoll.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import Option from "./Option";
+import { useCreateTopic } from "~/store/slices/createTopicSlice";
 
 const CreatePoll = () => {
   const [options, setOptions] = useState(["Option #1", "Option #2"]);
+
+  const voptions = useCreateTopic((state) => state.voptions);
+  const removeOption = useCreateTopic((state) => state.rmVoptions);
 
   const addNewOptionHandler = () => {
     setOptions((prev) => [...prev, `Option #${prev.length + 1}`]);
@@ -11,9 +15,13 @@ const CreatePoll = () => {
 
   const removeOptionHandler = (index: number) => {
     setOptions((prev) => prev.filter((p, i) => i !== index));
+    if (voptions.has(index)) {
+      removeOption(index);
+    }
   };
 
   const showCloseCondition = options.length > 2;
+  const disableButtonCondition = voptions.size < 2;
 
   return (
     <div className="  mt-2 bg-base-100 px-4 py-2">
@@ -32,7 +40,7 @@ const CreatePoll = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="h-4 w-4"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -49,14 +57,23 @@ const CreatePoll = () => {
           )}
         </div>
       ))}
-      <div className="flex items-center">
-        <button
-          className=" btn-sm btn-circle btn my-1"
-          onClick={() => addNewOptionHandler()}
-        >
-          <Image src="images/plus.svg" alt="plus" width={25} height={25} />
-        </button>
-        <span className="ml-2 text-gray-500">click to add another option</span>
+      <div className="mt-3 flex items-center justify-between">
+        <div className="flex items-center">
+          <button
+            className=" btn-sm btn-circle btn my-1"
+            onClick={() => addNewOptionHandler()}
+          >
+            <Image src="images/plus.svg" alt="plus" width={25} height={25} />
+          </button>
+          <span className="ml-2 text-gray-500">
+            click to add another option
+          </span>
+        </div>
+        <div>
+          <button className="btn-sm btn" disabled={disableButtonCondition}>
+            Create Poll
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/home/Option.tsx
+++ src/components/home/Option.tsx
@@ -21,16 +21,36 @@ const Option = ({ placeHolder, index }: Props) => {
   };
 
   return (
-    <div>
+    <div className="my-1 w-full">
       {added ? (
-        <p className="my-1">text</p>
+        <div className="flex w-full items-center justify-between">
+          <p className=" my-1">{text}</p>
+          <div className="ml-1">
+            <button
+              className="btn-sm btn-circle btn"
+              onClick={() => setAdded(false)}
+            >
+              <svg
+                className="h-4 w-4"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 20H20.5M18 10L21 7L17 3L14 6M18 10L8 20H4V16L14 6M18 10L14 6"
+                  stroke="#ffffff"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
       ) : (
         <input
           type="text"
           placeholder={placeHolder}
           value={text}
           onChange={(e) => setText(e.target.value)}
-          onBlur={() => addOption}
+          onBlur={() => addOption()}
           className="input-bordered input-secondary input input-sm my-1 w-full max-w-xs"
         />
       )}
